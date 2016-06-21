7e239c189d2a38e0aa7de70ef550f5d933fd6965
Ryan Latta <recursive.faults@gmail.com>
2016-06-21 15:34:41 -0500

    Add MacOS target for framework.

---
+++ ZidSwift-Mac/ZidSwift-Mac.h
@@ -0,0 +1,19 @@
+//
+//  ZidSwift-Mac.h
+//  ZidSwift-Mac
+//
+//  Created by Ryan Latta on 6/21/16.
+//  Copyright © 2016 ZidPlan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ZidSwift-Mac.
+FOUNDATION_EXPORT double ZidSwift_MacVersionNumber;
+
+//! Project version string for ZidSwift-Mac.
+FOUNDATION_EXPORT const unsigned char ZidSwift_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZidSwift_Mac/PublicHeader.h>
+
+
